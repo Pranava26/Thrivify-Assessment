19dd89f1a0b9d7351f042d3f308f306911dfeb03
Pranava26 <pranavasai26@gmail.com>
2024-12-22 16:33:21 +0530

    Fixed import type in backend

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -3,13 +3,17 @@ import path from 'path';
 import Sequelize from 'sequelize';
 import process from 'process';
 import { fileURLToPath, pathToFileURL } from 'url';
-import configFile from '../config/config.json' assert { type: 'json' };
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = configFile[env];
+
+// Use fs to read the JSON configuration file
+const config = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '../config/config.json'), 'utf-8')
+)[env];
+
 const db = {};
 
 let sequelize;
