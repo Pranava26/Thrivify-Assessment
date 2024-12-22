883913db3f9163bb77e70007b7c771ac5ee45ff9
Pranava26 <pranavasai26@gmail.com>
2024-12-22 22:18:23 +0530

    Changed flask backend route

--- flask_backend/app/routes.py
+++ flask_backend/app/routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, send_from_directory
 from .services.habit_suggestions import generate_habit_suggestions
-
+import os
 
 habit_routes = Blueprint('habit_routes', __name__)
 
@@ -8,3 +8,15 @@ habit_routes = Blueprint('habit_routes', __name__)
 def get_habit_suggestions():
     suggestions = generate_habit_suggestions()
     return jsonify(suggestions), 200
+
+
+habit_routes = Blueprint('habit_routes', __name__)
+
+# Serve React frontend
+@habit_routes.route('/', defaults={'path': ''})
+@habit_routes.route('/<path:path>')
+def serve_react_app(path):
+    react_build_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../frontend/dist'))
+    if path != "" and os.path.exists(os.path.join(react_build_dir, path)):
+        return send_from_directory(react_build_dir, path)
+    return send_from_directory(react_build_dir, 'index.html')
