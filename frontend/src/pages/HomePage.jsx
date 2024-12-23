1d2fae5594a8a7267f9df2354c4a338122fc941e
Pranava26 <pranavasai26@gmail.com>
2024-12-23 11:02:01 +0530

    Changed request path in frontend

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -25,7 +25,7 @@ const HomePage = () => {
   const fetchHabitSuggestions = async () => {
     setIsLoadingSuggestions(true);
     try {
-      const res = await axios.get('http://localhost:5000/generate-habit-suggestions');
+      const res = await axios.get('https://thrivify-assessment.onrender.com/generate-habit-suggestions');
       console.log(res);
       
       setSuggestions(res.data);
