722ef80549e1688ad80437042fcb0085bb211b00
Pranava26 <pranavasai26@gmail.com>
2024-12-23 13:00:11 +0530

    Changed flask backend logic

--- flask_backend/app/routes.py
+++ flask_backend/app/routes.py
@@ -6,4 +6,6 @@ habit_routes = Blueprint('habit_routes', __name__)
 @habit_routes.route('/generate-habit-suggestions', methods=['GET'])
 def get_habit_suggestions():
     suggestions = generate_habit_suggestions()
-    return jsonify(suggestions), 200
+    response = jsonify(suggestions)
+    response.headers['Content-Type'] = 'application/json'
+    return response, 200
